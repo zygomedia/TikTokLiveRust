b05260d275bc86dfa947398bbb365ceadeac1f49
Gremious Grenald <gremious@gremy.co.uk>
2024-04-18 15:36:52 +0100

    More cleaning more scrubbing

--- build-script/src/event_models_loader.rs
+++ build-script/src/event_models_loader.rs
@@ -6,8 +6,7 @@ use proc_macro2::{Ident, TokenStream};
 use quote::{quote, ToTokens};
 use syn::{Field, Fields, File, Item};
 
-pub struct TikTokEventDataModel
-{
+pub struct TikTokEventDataModel {
     pub is_webcast: bool,
     pub webcast_name: String,
     pub enum_name: String,
@@ -24,31 +23,24 @@ pub struct TikTokEventDataModel
 }
 
 
-pub fn get_event_data_models() -> Vec<TikTokEventDataModel>
-{
+pub fn get_event_data_models() -> Vec<TikTokEventDataModel> {
     let mut webcast_models = get_models_from_proto();
     let custom_models = get_models_from_custom_event();
 
-
-    for custom_model in custom_models
-    {
+    for custom_model in custom_models {
         let custom_model_name =  &custom_model.0.to_string();
         let custom_model_fields= custom_model.1.fields.clone();
-        if !webcast_models.contains_key(custom_model_name)
-        {
+        if !webcast_models.contains_key(custom_model_name) {
             webcast_models.insert(custom_model.0, custom_model.1);
             continue;
         }
 
-        match webcast_models.get_mut(custom_model_name)
-        {
-            Some(mut value) =>
-                {
-                    for field in custom_model_fields
-                    {
-                        value.fields.push(field.clone());
-                    }
-                }
+        match webcast_models.get_mut(custom_model_name) {
+            Some(value) => {
+				for field in custom_model_fields {
+					value.fields.push(field.clone());
+				}
+			}
             None => println!("'key3' is not in the map"),
         }
     }
@@ -64,14 +56,11 @@ fn remove_duplicates(models: &mut Vec<TikTokEventDataModel>) {
 }
 
 
-fn get_models_from_proto() -> HashMap<String, TikTokEventDataModel>
-{
+fn get_models_from_proto() -> HashMap<String, TikTokEventDataModel> {
     let syntax_tree = get_syntax_tree("src/proto/webcast.rs".to_string());
-    let mut eventModels = HashMap::new();
-    for item in syntax_tree.items
-    {
-        if let Item::Struct(struct_item) = item
-        {
+    let mut event_models = HashMap::new();
+    for item in syntax_tree.items {
+        if let Item::Struct(struct_item) = item {
             let webcast_name = struct_item.ident.to_string();
             let raw_name = webcast_name.clone()
                 .replace("Webcast", "")
@@ -79,48 +68,44 @@ fn get_models_from_proto() -> HashMap<String, TikTokEventDataModel>
 
             let enum_name = format!("On{}", raw_name);
             let event_name = format!("TikTok{}Event", raw_name).to_string();
-            let method_name = camel_to_snake_case(enum_name.clone().as_str());
+            let method_name_snake = camel_to_snake_case(enum_name.clone().as_str());
 
             let event_name_ident = Ident::new(&*event_name, proc_macro2::Span::call_site());
             let webcast_name_ident = Ident::new(&*webcast_name, proc_macro2::Span::call_site());
             let enum_name_ident = Ident::new(&*enum_name, proc_macro2::Span::call_site());
-            let method_name_ident = Ident::new(&*method_name, proc_macro2::Span::call_site());
+            let method_name_ident = Ident::new(&*method_name_snake, proc_macro2::Span::call_site());
 
 
-            let methodName = quote!
-            {
+            let method_name_token = quote! {
                  pub raw_data: #webcast_name_ident,
             };
 
             let model = TikTokEventDataModel
             {
-                fields: vec![methodName],
+                fields: vec![method_name_token],
                 is_webcast: true,
                 webcast_name,
                 enum_name,
                 event_name: event_name.clone(),
                 raw_name,
-                method_name,
+                method_name: method_name_snake,
 
                 webcast_name_ident,
                 enum_name_ident,
                 event_name_ident,
                 method_name_ident,
             };
-            eventModels.insert(event_name.clone().to_string(), model);
+            event_models.insert(event_name.clone().to_string(), model);
         }
     }
-    return eventModels;
+    return event_models;
 }
 
-fn get_models_from_custom_event() -> HashMap<String, TikTokEventDataModel>
-{
+fn get_models_from_custom_event() -> HashMap<String, TikTokEventDataModel> {
     let syntax_tree = get_syntax_tree("src/event_models.rs".to_string());
-    let mut eventModels = HashMap::new();
-    for item in syntax_tree.items
-    {
-        if let Item::Struct(struct_item) = item
-        {
+    let mut event_models = HashMap::new();
+    for item in syntax_tree.items {
+        if let Item::Struct(struct_item) = item {
             let name = struct_item.ident.to_string();
             let raw_name = name.clone()
                 .replace("TikTok", "")
@@ -140,8 +125,7 @@ fn get_models_from_custom_event() -> HashMap<String, TikTokEventDataModel>
                 Fields::Unnamed(fields) => format_fields(fields.unnamed.into_iter()),
                 Fields::Unit => quote! {}, // For unit structs, which have no fields
             };
-            let model = TikTokEventDataModel
-            {
+            let model = TikTokEventDataModel {
                 fields: vec![fields_tokenstream],
                 is_webcast: false,
                 webcast_name: name,
@@ -155,15 +139,13 @@ fn get_models_from_custom_event() -> HashMap<String, TikTokEventDataModel>
                 event_name_ident,
                 method_name_ident,
             };
-            eventModels.insert(event_name.clone().to_string(), model);
+            event_models.insert(event_name.clone().to_string(), model);
         }
     }
-    return eventModels;
+    return event_models;
 }
 
-//""
-fn get_syntax_tree(path: String) -> File
-{
+fn get_syntax_tree(path: String) -> File {
     let path = Path::new(path.as_str());
     let code = fs::read_to_string(path).expect("Unable to read the file");
     let syntax_tree = syn::parse_file(&code).expect("Unable to parse file");
